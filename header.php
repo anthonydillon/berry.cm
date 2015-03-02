0a16fdffe1bb1ee8373d0ca29bba940d8a50d443
Anthony Dillon <me@anthonydillon.com>
2015-03-02 23:27:57 +0000

    Begun developing the new design

--- header.php
+++ header.php
@@ -8,17 +8,16 @@
  * @subpackage Sherpa
  */
 ?><!DOCTYPE html>
-<html <?php language_attributes(); ?> class="no-js">
+<html <?php language_attributes(); ?>>
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width">
-	<title>Sherpa</title>
+	<title>Berry.co: The personal shopper for amazon</title>
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 	<!--[if lt IE 9]>
 	<script src="<?php echo esc_url( get_template_directory_uri() ); ?>/js/html5.js"></script>
 	<![endif]-->
-	<script>(function(){document.documentElement.className='js'})();</script>
 	<link rel="stylesheet" href="<?php bloginfo('template_directory');?>/css/normalize.css">
   	<link rel="stylesheet" href="<?php bloginfo('template_directory');?>/css/skeleton.css">
   	<link rel="stylesheet" href="<?php bloginfo('template_directory');?>/css/custom.css">
@@ -29,3 +28,27 @@
 <body <?php body_class(); ?>>
 <div class="container">
 	<div id="content" class="site-content">
+		<header role="banner">
+			<div class="eight columns">
+				<a href="/" class="brand">
+					<span class="logo"></span>
+					<h5 class="name"><?php bloginfo( 'name' ); ?></h5>
+				</a>
+			</div>
+			<div class="four columns">
+				<p class="tag u-pull-right">The personal shopper for amazon</p>
+			</div>
+
+			<nav role="navigation">
+				<h3 class="question">I'm looking for a gift for </h3>
+				<select class="gift-for">
+					<option value="">everyone</option>
+					<?php
+					$categories = get_categories();
+					foreach ($categories as $category) {
+						echo '<option value="'.$category->slug.'">'.$category->cat_name.'</option>';
+					}
+					?>
+				</select>
+			</nav>
+		</header>
--- index.php
+++ index.php
@@ -8,23 +8,12 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-			<h1 class="title"><?php bloginfo( 'name' ); ?></h1>
-			<h3 class="question">Who are you shopping for?</h3>
 			<div class="row">
 				<div class="two columns">&nbsp;</div>
 				<div class="eight columns">
+
 					<ul class="float-list">
-						<?php
-						$categories = get_categories();
-						foreach ($categories as $category) {
-							$listitem = '<li class="list-item">';
-							$listitem .= '<a class="button"  href="/sherpa/'.$category->slug.'">';
-							$listitem .= $category->cat_name;
-							$listitem .= '</a>';
-							$listitem .= '</li>';
-							echo $listitem;
-						}
-						?>
+
 					</ul>
 				</div>
 				<div class="two columns">&nbsp;</div>
