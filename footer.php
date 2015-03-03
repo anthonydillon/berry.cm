5ecee60a817951b85862945f683b1ef24178bf3a
Anthony Dillon <me@anthonydillon.com>
2015-03-03 23:05:23 +0000

    Complete the rest of the homepage layout

--- footer.php
+++ footer.php
@@ -12,9 +12,19 @@
 	</div><!-- .site-content -->
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
-		<div class="site-info">
-
-		</div><!-- .site-info -->
+		<div class="row">
+			<div class="three columns">&nbsp;</div>
+			<div class="six columns site-info">
+				<p>Find the perfect gifts for everyone with the personal shopper for Amazon.</p>
+				<ul class="social-list">
+					<li class="social-list__item"><a href=""><img src="<?php bloginfo('template_directory'); ?>/images/icons/icon-twitter.png" alt="Twitter" width="32" height="32" /></a></li>
+					<li class="social-list__item"><a href=""><img src="<?php bloginfo('template_directory'); ?>/images/icons/icon-facebook.png" alt="Facebook" width="32" height="32" /></a></li>
+					<li class="social-list__item"><a href=""><img src="<?php bloginfo('template_directory'); ?>/images/icons/icon-instagram.png" alt="Instagram" width="32" height="32" /></a></li>
+					<li class="social-list__item"><a href=""><img src="<?php bloginfo('template_directory'); ?>/images/icons/icon-pinterest.png" alt="Pinterest" width="32" height="32" /></a></li>
+				</ul>
+				<p class="copyright">Sherpa is an independent company. Amazon is a trademarked brand. ©2015 Sherpa.io. All rights reserved.</p>
+			</div><!-- .site-info -->
+		</div>
 	</footer><!-- .site-footer -->
 
 </div><!-- .site -->
--- index.php
+++ index.php
@@ -9,14 +9,25 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 			<div class="row">
-				<div class="two columns">&nbsp;</div>
-				<div class="eight columns">
-
-					<ul class="float-list">
-
-					</ul>
-				</div>
-				<div class="two columns">&nbsp;</div>
+				<ul class="products">
+				<?php
+					$args = array( 'post_type' => 'product', 'orderby' => 'rand' );
+					$rand_posts = get_posts( $args );
+					$i = 0;
+					foreach ( $rand_posts as $post ) :
+						setup_postdata( $post ); ?>
+						<li class="product-item four columns <?php if ($i % 3 == 0) { echo 'first-col'; } ?>">
+							<a href="<?php the_permalink(); ?>" class="product-item__button button">
+								<p class="product-item__name"><?php the_title(); ?></p>
+								<img src="http://dummyimage.com/300x100/ccc/fff.png&amp;text=+" alt="" class="product-item__image" />
+								<p class="product-item__price">&pound;32.99</p>
+							</a>
+						</li>
+						<?php
+						$i++;
+					endforeach;
+					wp_reset_postdata(); ?>
+				</ul>
 			</div>
 		</main><!-- .site-main -->
 	</div><!-- .content-area -->
